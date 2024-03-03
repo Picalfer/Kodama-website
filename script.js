693dbd41c7783ebcd5ef836a33b81ec2efa92ee6
Arthur Kozhemyakin <trable228@icloud.com>
2024-03-03 18:01:51 +0300

    faq section

--- script.js
+++ script.js
@@ -40,3 +40,12 @@ document.addEventListener('mousemove', function(event) {
   });
 });
 
+function toggleAnswer(id) {
+  var answer = document.getElementById(id);
+  if (answer.style.display === 'none' || answer.style.display === '') {
+      answer.style.display = 'block';
+  } else {
+      answer.style.display = 'none';
+  }
+}
+
