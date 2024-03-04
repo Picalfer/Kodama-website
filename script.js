75e9a03bd5bf6e0aa05b5f61ecb3cf6cb80eaf8e
Arthur Kozhemyakin <trable228@icloud.com>
2024-03-04 18:32:30 +0300

    refactor, to faq add questions

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Получаем все кнопки меню
-const menuButtons = document.querySelectorAll('.header-items a');
+const menuButtons = document.querySelectorAll('.menu a');
 const selectCourseButton = document.getElementById('select-course-btn');
 
 // Для каждой кнопки меню добавляем обработчик события при нажатии
 menuButtons.forEach(button => {
-  button.addEventListener('click', function(e) {
+  button.addEventListener('click', function (e) {
     e.preventDefault(); // Предотвращаем стандартное поведение ссылки
 
     // Получаем id раздела, к которому нужно прокрутить страницу
@@ -18,40 +18,43 @@ menuButtons.forEach(button => {
   });
 });
 
-selectCourseButton.addEventListener('click', function(event) {
-    event.preventDefault();
+selectCourseButton.addEventListener('click', function (event) {
+  event.preventDefault();
 
-    const targetElement = document.getElementById('courses');
+  const targetElement = document.getElementById('courses');
 
-    targetElement.scrollIntoView({ behavior: 'smooth' });
+  targetElement.scrollIntoView({ behavior: 'smooth' });
 });
 
-document.addEventListener('mousemove', function(event) {
+document.addEventListener('mousemove', function (event) {
   const squares = document.querySelectorAll('.square');
 
-  squares.forEach(function(square) {
-      const speed = parseInt(square.getAttribute('data-speed'));
-      const x = (window.innerWidth - event.pageX * speed) / 100;
-      const y = (window.innerHeight - event.pageY * speed) / 100;
+  squares.forEach(function (square) {
+    const speed = parseInt(square.getAttribute('data-speed'));
+    const x = (window.innerWidth - event.pageX * speed) / 100;
+    const y = (window.innerHeight - event.pageY * speed) / 100;
 
-      square.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+    square.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
   });
 });
 
 function toggleAnswer(id) {
   var answer = document.getElementById(id);
   if (answer.style.display === 'none' || answer.style.display === '') {
-      answer.style.display = 'block';
+    answer.style.display = 'block';
   } else {
-      answer.style.display = 'none';
+    answer.style.display = 'none';
   }
 }
 
 const bubbles = document.querySelectorAll('.bubble');
 
-    bubbles.forEach(bubble => {
-        const size = Math.floor(Math.random() * 10) + 2;
-        bubble.style.width = size + 'vw';
-        bubble.style.height = size + 'vh';
-    });
+bubbles.forEach(bubble => {
+  const size = Math.floor(Math.random() * 10) + 2;
+  bubble.style.width = size + 'vw';
+  bubble.style.height = size + 'vh';
+});
 
+document.getElementById('menu-toggle').addEventListener('click', function () {
+  document.querySelector('nav.menu').classList.toggle('open');
+});
