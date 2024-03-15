67c84f640bf23723641bba280288d15bac8a3f0d
Arthur Kozhemyakin <trable228@icloud.com>
2024-03-15 20:36:39 +0300

    refactore to adaptive mobile version

--- script.js
+++ script.js
@@ -56,17 +56,19 @@ selectCourseButton.addEventListener('click', function (event) {
   targetElement.scrollIntoView({ behavior: 'smooth' });
 });
 
-document.addEventListener('mousemove', function (event) {
-  const squares = document.querySelectorAll('.square');
-
-  squares.forEach(function (square) {
-    const speed = parseInt(square.getAttribute('data-speed'));
-    const x = (window.innerWidth - event.pageX * speed) / 100;
-    const y = (window.innerHeight - event.pageY * speed) / 100;
-
-    square.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+if (window.innerWidth > 600) {
+  document.addEventListener('mousemove', function (event) {
+    const squares = document.querySelectorAll('.square');
+  
+    squares.forEach(function (square) {
+      const speed = parseInt(square.getAttribute('data-speed'));
+      const x = (window.innerWidth - event.pageX * speed) / 100;
+      const y = (window.innerHeight - event.pageY * speed) / 100;
+  
+      square.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+    });
   });
-});
+}
 
 function toggleAnswer(id) {
   var answer = document.getElementById(id);
