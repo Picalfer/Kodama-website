42a40345535422ff49f1703488224439d4ec4a98
Arthur Kozhemyakin <trable228@icloud.com>
2024-03-17 19:15:42 +0300

    refactor header logo link, cant scroll when open mobile menu, change design submit app section

--- script.js
+++ script.js
@@ -154,3 +154,96 @@ window.addEventListener('scroll', function () {
     });
   }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  var menuToggle = document.getElementById('open-menu-btn');
+  var mobileMenu = document.getElementById('mobile-menu');
+  var closeButton = document.getElementById('close-menu-btn');
+  var menuButton = document.getElementById('open-menu-btn');
+  var navLinks = document.querySelectorAll('#nav-mobile-menu a');
+
+  function closeMenu() {
+    mobileMenu.style.display = 'none';
+    menuButton.style.display = 'block';
+    document.body.classList.remove('no-scroll'); // Удаляем класс no-scroll
+  }
+
+  function openMenu() {
+    mobileMenu.style.display = 'block';
+    menuButton.style.display = 'none';
+    document.body.classList.add('no-scroll'); // Добавляем класс no-scroll
+  }
+
+  menuToggle.addEventListener('click', function () {
+    openMenu();
+  });
+
+  closeButton.addEventListener('click', function () {
+    closeMenu();
+  });
+
+  mobileLogo.addEventListener('click', function () {
+    closeMenu();
+  })
+
+  navLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+      closeMenu();
+    });
+  });
+});
+
+// Получаем элементы формы
+const courseSelect = document.getElementById('course-select');
+const submitForm = document.getElementById('submit-app-content');
+const formInputs = document.querySelectorAll('#submit-app-form input');
+
+// Добавляем обработчик события change
+courseSelect.addEventListener('change', changeBackground);
+
+// Функция для изменения фона в зависимости от выбранного варианта
+function changeBackground() {
+  const selectedCourse = courseSelect.value;
+
+  // Определяем цвет фона в зависимости от выбранного курса
+  let backgroundColor;
+  let inputBackgroundColor;
+  let backgroundColorTransparent;
+  switch (selectedCourse) {
+    case 'figma':
+      backgroundColor = '#FFC75C'; // Желтый цвет для Figma
+      inputBackgroundColor = '#FFFFE0'; // Более блеклый цвет для областей ввода информации
+      backgroundColorTransparent = 'rgba(255, 199, 92, 0.5)';
+      break;
+    case 'computer-literacy':
+      backgroundColor = '#41EAD4'; // Зеленый цвет для Компьютерной грамотности
+      inputBackgroundColor = '#E0FFF3'; // Более блеклый цвет для областей ввода информации
+      backgroundColorTransparent = 'rgba(65, 234, 212, 0.5)';
+      break;
+    case 'scratch':
+      backgroundColor = '#6E65FF'; // Фиолетовый цвет для Scratch
+      inputBackgroundColor = '#F0E7FF'; // Более блеклый цвет для областей ввода информации
+      backgroundColorTransparent = 'rgba(110, 101, 255, 0.5)';
+      break;
+    case 'roblox':
+      backgroundColor = '#FF6E6E'; // Красный цвет для Roblox
+      inputBackgroundColor = '#FFE0E0'; // Более блеклый цвет для областей ввода информации
+      backgroundColorTransparent = 'rgba(255, 110, 110, 0.5)';
+      break;
+    default:
+      backgroundColor = '#ffffff'; // Белый цвет по умолчанию
+      inputBackgroundColor = '#f8f9fa'; // Светло-серый цвет для областей ввода информации
+      backgroundColorTransparent = 'rgba(255, 255, 255, 0.5)';
+  }
+
+  // Устанавливаем цвет фона для элемента выбора курса
+  courseSelect.style.backgroundColor = backgroundColor;
+
+  // Устанавливаем цвет фона для всей формы с изменением прозрачности
+  submitForm.style.backgroundColor = backgroundColorTransparent;
+
+  // Устанавливаем цвет фона для областей ввода информации
+  formInputs.forEach(input => {
+    input.style.backgroundColor = inputBackgroundColor;
+  });
+}
