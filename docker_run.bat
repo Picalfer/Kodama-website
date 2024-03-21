51df6188b9f721a5943aa696967af65d4909abb2
Arthur Kozhemyakin <trable228@icloud.com>
2024-03-21 16:40:52 +0300

    fix icon

--- docker_run.bat
+++ docker_run.bat
@@ -1,9 +1,9 @@
-@echo off
-REM Сборка Docker-образа
-docker build .
+docker stop $(docker ps -a -q) # Stop all running containers
+docker rm $(docker ps -a -q) # Remove all containers
+docker rmi my_image # Remove the image named my_image
 
-REM Вывод списка образов и их хешей
-docker image ls --format "{{.ID}}"
+REM Сборка Docker-образа
+docker build -t my_image .
 
 REM Извлечение хеша из списка образов
 for /f "tokens=1" %%a in ('docker image ls --format "{{.ID}}"') do set docker_hash=%%a
@@ -12,4 +12,4 @@ REM Извлечение первых 5 символов из хеша
 set docker_hash=%docker_hash:~0,5%
 
 REM Запуск Docker-контейнера с полученным хешем
-docker run -d -p 5050:5050 %docker_hash%
+docker run -d -p 5050:5050 my_image
